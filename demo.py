f26fd9ad18c286bee736140353cbcef8fbe4d4fd
chLi <565353780@qq.com>
2023-02-02 13:00:36 +0800

    add basic excel manager class

---
+++ demo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from excel_manage.Demo.excel_manager import demo as demo_manage_excel
+
+if __name__ == "__main__":
+    demo_manage_excel()
---
+++ excel_manage/Demo/excel_manager.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from excel_manage.Module.excel_manager import ExcelManager
+
+
+def demo():
+    excel_manager = ExcelManager()
+    return True
