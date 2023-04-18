c4760c3fdc965a70fa6591ca6a2f025130f1b1bb
chLi <565353780@qq.com>
2023-04-18 19:49:22 +0800

    add title list config

---
+++ excel_manage/Config/title.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+TITLE_LIST = ['时间', '类别', '剩余次数', '剩余时间']
--- excel_manage/Method/io.py
+++ excel_manage/Method/io.py
@@ -6,11 +6,13 @@ import os
 import xlrd
 import xlwt
 
+from excel_manage.Config.title import TITLE_LIST
 from excel_manage.Method.path import createFileFolder, removeFile, renameFile
 
 
-def createExcel(excel_file_path, sheet_name='Sheet 0', title_list=[]):
+def createExcel(excel_file_path, sheet_name='Sheet 0', title_list=TITLE_LIST):
     workbook = xlwt.Workbook(encoding='utf-8')
+    #  workbook = xlwt.Workbook()
     worksheet = workbook.add_sheet(sheet_name)
     for i, title in enumerate(title_list):
         worksheet.write(1, i, title)
